0ff61dc210d7a910e699f862b2475db810d97e37
Andrii <andrejsalogub3@gmail.com>
2024-02-12 14:36:17 +0200

    Added temperature scale selection functionality.

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,31 +1,49 @@
 import { Weather, WeatherData } from "./types";
 
-const apiKey = "5c5fc4bb391bf0a314053ba9c7aa8cd3";
+const apiKey: string = "5c5fc4bb391bf0a314053ba9c7aa8cd3";
+let currentTemperatureScale: "celsius" | "fahrenheit" = "celsius";
 
 const currentWeather = document.querySelector(".weather__current");
-const hourlyWeather = document.querySelector(".weather__interval-forecast");
+const intervalWeather = document.querySelector(".weather__interval-forecast");
 
 const searchInput = document.querySelector(".header__search-input");
 const searchBtn = document.querySelector(".header__search-btn");
-let searchTerm: string = "";
+let searchTerm: string = "Kharkiv";
+
+const celsiusScale = document.querySelector("#celsius");
+const fahrenheitScale = document.querySelector("#fahrenheit");
 
 const displayCurrentWeather = (data: Weather, city: string) => {
     const { temp, temp_max, temp_min } = data.main;
     const weather = data.weather[0].main;
 
+    let temperatureInCurrentScale: number = temp - 273;
+    let maxTemperatureScale: number = temp_max - 273;
+    let minTemperatureScale: number = temp_min - 273;
+
+    if (currentTemperatureScale === "fahrenheit") {
+        temperatureInCurrentScale = (temperatureInCurrentScale * 9) / 5 + 32;
+        maxTemperatureScale = (maxTemperatureScale * 9) / 5 + 32;
+        minTemperatureScale = (minTemperatureScale * 9) / 5 + 32;
+    }
+
     if (currentWeather) {
         currentWeather.innerHTML = `<h2 class="weather__current-city">${city}</h2>
         <h3 class="weather__current-weather">${weather}</h3>
-        <p class="weather__current-temperature">${(temp - 273).toFixed(0)}°</p>
+        <p class="weather__current-temperature">${temperatureInCurrentScale.toFixed(
+            0
+        )}°</p>
         <p class="weather__current-max-min-temperature">
-            H:${(temp_max - 273).toFixed(0)}° L:${(temp_min - 273).toFixed(0)}°
+            H:${maxTemperatureScale.toFixed(
+                0
+            )}° L:${minTemperatureScale.toFixed(0)}°
         </p>`;
     }
 };
 
 const displayHourlyForecast = (data: WeatherData) => {
-    if (hourlyWeather) {
-        hourlyWeather.innerHTML = "";
+    if (intervalWeather) {
+        intervalWeather.innerHTML = "";
     }
 
     for (let i = 0; i <= 8; i++) {
@@ -44,12 +62,14 @@ const displayHourlyForecast = (data: WeatherData) => {
                 alt="weather  image"
                 class="weather__interval-forecast-image"
             />
-            <p class="weather__interval-forecast-temperature">${(
-                temp - 273
-            ).toFixed(0)}°</p>
+            <p class="weather__interval-forecast-temperature">${
+                currentTemperatureScale === "celsius"
+                    ? (temp - 273).toFixed(0)
+                    : (((temp - 273) * 9) / 5 + 32).toFixed(0)
+            }°</p>
         `;
 
-        hourlyWeather?.append(li);
+        intervalWeather?.append(li);
     }
 };
 
@@ -87,3 +107,24 @@ searchBtn?.addEventListener("click", (e: Event) => {
     e.preventDefault();
     fetchData(searchTerm, apiKey);
 });
+celsiusScale?.addEventListener("click", () => {
+    if (currentTemperatureScale !== "celsius") {
+        currentTemperatureScale = "celsius";
+        fahrenheitScale?.classList.remove("header__temperature-scale_current");
+        celsiusScale.classList.add("header__temperature-scale_current");
+        updateWeatherDisplay();
+    }
+});
+
+fahrenheitScale?.addEventListener("click", () => {
+    if (currentTemperatureScale !== "fahrenheit") {
+        currentTemperatureScale = "fahrenheit";
+        celsiusScale?.classList.remove("header__temperature-scale_current");
+        fahrenheitScale.classList.add("header__temperature-scale_current");
+        updateWeatherDisplay();
+    }
+});
+
+function updateWeatherDisplay() {
+    fetchData(searchTerm, apiKey);
+}
